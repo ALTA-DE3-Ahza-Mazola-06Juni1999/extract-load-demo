a3fcbfdab0e8dbe530bc7a3a0588f4a87a9692e9
Ahza Mazola <ahzamazola@gmail.com>
2024-07-14 16:05:22 +0700

    commit extract and load

--- bigquery/load.py
+++ bigquery/load.py
@@ -1,21 +1,24 @@
 from google.cloud import bigquery
 import os
 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/root/.config/gcloud/application_default_credentials.json'
 
 def write_to_bigquery(table_id, rows_to_insert):
-    client = bigquery.Client()
+    client = bigquery.Client(project='project-428209')
     table = client.get_table(table_id)
-    errors = client.insert_rows(table, rows_to_insert)
+    errors = client.insert_rows_json(table, rows_to_insert)
     if errors:
         print('Encountered errors while inserting rows: {}'.format(errors))
     else:
-        print('Successfully insert data')
+        print('Successfully inserted data')
 
-
-project_id = os.getenv('PROJECT_ID')
-table_id = f'{project_id}.my_dataset.my_table'
+table_id = 'project-428209.project_zola.my_table'
 rows_to_insert = [
-    (1, 'Phred Phlyntstone', '2023-11-10 08:00:00'),
-    (2, 'Wylma Phlyntstone', '2023-11-10 08:00:00')
+    {"id": 1, "name": 'Phred Phlyntstone', "created_at": '2023-11-10 08:00:00'},
+    {"id": 2, "name": 'Wylma Phlyntstone', "created_at": '2023-11-10 08:00:00'},
+    {"id": 3, "name": 'Wylma', "created_at": '2023-11-10 10:00:00'},
+    {"id": 4, "name": 'Phlyntstone', "created_at": '2023-11-10 11:00:00'},
+    {"id": 5, "name": 'maPhlyn', "created_at": '2023-11-10 12:00:00'}
 ]
+
 write_to_bigquery(table_id, rows_to_insert)
